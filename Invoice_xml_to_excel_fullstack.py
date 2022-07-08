3ca791f086864648d34473ad67280725f61602d2
Hayk Asatryan <hayk.asatryan@easypay.local>
2022-07-08 11:53:12 +0400

    Bug fixes

--- Invoice_xml_to_excel_fullstack.py
+++ Invoice_xml_to_excel_fullstack.py
@@ -4,17 +4,17 @@ import pandas as pd
 import sys
 
 sg.theme("green")
-sg.set_options(font = "Arial", element_text_color="white", text_color="white")
+sg.set_options(font="Arial", element_text_color="white", text_color="white")
 
 mainlayout = [
     [sg.Text("Խնդրում եմ ներմուծել eInvoice-ից արտահանված xml ֆայլը")],
     [sg.InputText(key="-XML_PATH-", disabled=True), sg.FileBrowse(file_types=[('XML Files', '*.xml')])],
     [sg.Text("Խնդրում եմ նշել թե որտեղ արտահանել excel ֆայլը")],
     [sg.InputText(key="-XLSX_PATH-", disabled=True), sg.FolderBrowse()],
-    [sg.Button("Արտահանել Excel-ը",key="-RUN-", size=(53,1))]
+    [sg.Button("Արտահանել Excel-ը", key="-RUN-", size=(53, 1))]
 ]
 
-MainWindow = sg.Window("XML Invoice to Excel",mainlayout)
+MainWindow = sg.Window("XML Invoice to Excel", mainlayout)
 
 while True:
     event, values = MainWindow.read()
@@ -30,105 +30,105 @@ while True:
         xml_path = values['-XML_PATH-']
         xlsx_path = values['-XLSX_PATH-']
         xlsx_path = xlsx_path + '/invoices.xlsx'
-        break
-try:
-    tree = et.parse(xml_path)
+        try:
+            tree = et.parse(xml_path)
 
-    root = tree.getroot()
+            root = tree.getroot()
 
-    invoice_serial = []
-    invoice_number = []
-    status = []
-    recorded_id = []
-    issue_date = []
-    delivery_date = []
-    payment_date = []
-    tax_code = []
-    invoice_amount = []
-    invoice_description = []
-    standard_cost_id = []
-    users = []
-    comment = []
+            invoice_serial = []
+            invoice_number = []
+            status = []
+            recorded_id = []
+            issue_date = []
+            delivery_date = []
+            payment_date = []
+            tax_code = []
+            invoice_amount = []
+            invoice_description = []
+            standard_cost_id = []
+            users = []
+            comment = []
 
-    for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}Series"):
-        invoice_serial.append(elm.text)
+            for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}Series"):
+                invoice_serial.append(elm.text)
 
-    for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}Number"):
-        invoice_number.append(elm.text)
+            for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}Number"):
+                invoice_number.append(elm.text)
 
-    for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}Series"):
-        status.append('Դուրս գրված')
-        recorded_id.append('Ոչ')
-        users.append('Անուն Ազգանուն')
-        invoice_description.append('Գրեք')
-        payment_date.append(0)
-        standard_cost_id.append(0)
-        comment.append(0)
+            for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}Series"):
+                status.append('Դուրս գրված')
+                recorded_id.append('Ոչ')
+                users.append('Անուն Ազգանուն')
+                invoice_description.append('Գրեք')
+                payment_date.append(0)
+                standard_cost_id.append(0)
+                comment.append(0)
 
-    for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}SubmissionDate"):
-        issue_date.append(elm.text)
+            for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}SubmissionDate"):
+                issue_date.append(elm.text)
 
-    if invoice_serial[0] == 'Բ':
-        for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}DeliveryDate"):
-            delivery_date.append(elm.text)
-    else:
-        for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}SupplyDate"):
-            delivery_date.append(elm.text)
+            if invoice_serial[0] == 'Բ':
+                for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}DeliveryDate"):
+                    delivery_date.append(elm.text)
+            else:
+                for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}SupplyDate"):
+                    delivery_date.append(elm.text)
 
-    for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}SupplierInfo"):
-        elm = elm.find(".//{http://www.taxservice.am/tp3/invoice/definitions}TIN")
-        tax_code.append(elm.text)
+            for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}SupplierInfo"):
+                elm = elm.find(".//{http://www.taxservice.am/tp3/invoice/definitions}TIN")
+                tax_code.append(elm.text)
 
+            for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}Total"):
+                elm = elm.find(".//{http://www.taxservice.am/tp3/invoice/definitions}TotalPrice")
+                invoice_amount.append(elm.text)
 
-    for elm in root.findall(".//{http://www.taxservice.am/tp3/invoice/definitions}Total"):
-        elm = elm.find(".//{http://www.taxservice.am/tp3/invoice/definitions}TotalPrice")
-        invoice_amount.append(elm.text)
+            output = {
+                'invoice_serial': invoice_serial,
+                'invoice_number': invoice_number,
+                'status': status,
+                'recorded_id': recorded_id,
+                'issue_date': issue_date,
+                'delivery_date': delivery_date,
+                'payment_date': payment_date,
+                'tax_code': tax_code,
+                'invoice_description': invoice_description,
+                'standard_cost_id': standard_cost_id,
+                'users': users,
+                'comment': comment
+            }
 
-    output = {
-        'invoice_serial': invoice_serial,
-        'invoice_number': invoice_number,
-        'status' : status,
-        'recorded_id': recorded_id,
-        'issue_date': issue_date,
-        'delivery_date': delivery_date,
-        'payment_date': payment_date,
-        'tax_code': tax_code,
-        'invoice_description': invoice_description,
-        'standard_cost_id': standard_cost_id,
-        'users': users,
-        'comment': comment
-    }
+            pd.set_option('display.max_columns', None)
+            df = pd.DataFrame(output)
 
-    pd.set_option('display.max_columns', None)
-    df = pd.DataFrame(output)
+            df['Invoice ID'] = df['invoice_serial'] + df['invoice_number'].astype(str)
+            df['issue_date'] = df['issue_date'].str[:10]
+            df['issue_date'] = pd.to_datetime(df['issue_date']).dt.strftime('%d/%m/%Y')
+            df['delivery_date'] = df['delivery_date'].str[:10]
+            df['delivery_date'] = pd.to_datetime(df['delivery_date']).dt.strftime('%d/%m/%Y')
+            df['payment_date'].replace([0], '', inplace=True)
+            df['standard_cost_id'].replace([0], '', inplace=True)
+            df['comment'].replace([0], '', inplace=True)
 
-    df['Invoice ID'] = df['invoice_serial'] + df['invoice_number'].astype(str)
-    df['issue_date'] = df['issue_date'].str[:10]
-    df['issue_date'] = pd.to_datetime(df['issue_date']).dt.strftime('%d/%m/%Y')
-    df['delivery_date'] = df['delivery_date'].str[:10]
-    df['delivery_date'] = pd.to_datetime(df['delivery_date']).dt.strftime('%d/%m/%Y')
-    df['payment_date'].replace([0], '', inplace=True)
-    df['standard_cost_id'].replace([0], '', inplace=True)
-    df['comment'].replace([0], '', inplace=True)
+            df = df[[
+                'Invoice ID',
+                'status',
+                'recorded_id',
+                'issue_date',
+                'delivery_date',
+                'payment_date',
+                'tax_code',
+                'invoice_description',
+                'standard_cost_id',
+                'users',
+                'comment'
+            ]]
 
-    df = df[[
-        'Invoice ID',
-        'status',
-        'recorded_id',
-        'issue_date',
-        'delivery_date',
-        'payment_date',
-        'tax_code',
-        'invoice_description',
-        'standard_cost_id',
-        'users',
-        'comment'
-    ]]
-
-    writer = pd.ExcelWriter(xlsx_path)
-    df.to_excel(writer)
-    writer.save()
-    sg.popup("Բարեհաջող ավարտ", "Invoice-ները բարեհաջող արտահանվել են")
-    MainWindow.close()
-except Exception:
-    sg.popup("Անհաջող փորձ", "Invoice-ը չստացվեց արտահանել։ Invoice-ի ֆորմատը չի համապատասխանում ծրագրին։")
+            writer = pd.ExcelWriter(xlsx_path)
+            df.to_excel(writer)
+            writer.save()
+            sg.popup("Կատարված է", "Invoice-ները բարեհաջող արտահանվել են")
+            continue
+        except Exception:
+            sg.popup("Անհաջող փորձ", "Invoice-ը չստացվեց արտահանել։ Invoice-ի ֆորմատը չի համապատասխանում ծրագրին։"
+                                     "Խնդրի պատճառը չհայտնաբերելու պարագայում դիմեք Հայկին:")
+            continue
